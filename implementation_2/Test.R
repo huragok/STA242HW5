9e4ddf05b68bff6c1e714b4ec52afd0de9a156ff
wenhaowu@Loki <wenhaowu1989@hotmail.com>
2015-05-22 17:01:34 -0700

    Check and fix the matching between all data and fare files.

--- implementation_2/Test.R
+++ implementation_2/Test.R
@@ -4,16 +4,8 @@ library(NYCTaxi)
 library(hash)
 library(Hmisc)
 
-updateCount <- function(table_entry, hash_table) {
-  key <- table_entry
-  print(str(key))
-  has.key(key, hash_table) || (hash_table[[key]] <- 0)
-  hash_table[[key]] <- hash_table[[key]] + unname(table_entry)
-}
-
 idx_file <- 1L
-size_batch <- 500L
-#size_batch <- 5000000L
+size_batch <- 500000L
 cmd_parse_data <- paste("unzip -cq ../data/trip_data_", idx_file, ".csv.zip | cut -d , -f 9", sep = "")
 cmd_parse_fare <- paste("unzip -cq ../data/trip_fare_", idx_file, ".csv.zip | cut -d , -f 6,7,10", sep = "")
 
@@ -30,9 +22,9 @@ mat_reg1_XX_XY <- matrix(0, nrow = 2, ncol = 3) # Fare amount less the tolls vs 
 mat_reg2_XX_XY <- matrix(0, nrow = 3, ncol = 4) # Fare amount less the tolls vs trip time and surcharge
 
 Rprof("ProfNYCTaxiOriginal.out", line.profiling=TRUE) # Profiling the program
-#tryCatch(
-#  {
-#    while (TRUE) {
+tryCatch(
+  {
+    while (TRUE) {
       trip_time <- unlist(read.csv(connection_data, nrow=size_batch, header=FALSE, stringsAsFactors=FALSE), use.names = FALSE)
       fares <- data.matrix(read.csv(connection_fare, nrow=size_batch, header=FALSE, stringsAsFactors=FALSE))
       #print(trip_time)
@@ -43,34 +35,32 @@ Rprof("ProfNYCTaxiOriginal.out", line.profiling=TRUE) # Profiling the program
 
       # Update the decile using a histogram (package "hash")
       hist_bulk <- table(fare_less_toll)
-      updateCount(hist_bulk[1], hist)
-      apply(hist_bulk, 1, updateCount, hist)
-      #value <- names(hist_bulk)
-      #count <- as.vector(hist_bulk)
+      value <- names(hist_bulk)
+      count <- as.vector(hist_bulk)
       
-      #for (idx in seq_along(hist_bulk)) {
-      #  key <- value[idx]
-      #  (has.key(key, hist)) || (hist[[key]] <- 0)
-      #  hist[[key]] <- hist[[key]] + count[idx]
-      #}
+      for (idx in seq_along(hist_bulk)) {
+        key <- value[idx]
+        (has.key(key, hist)) || (hist[[key]] <- 0)
+        hist[[key]] <- hist[[key]] + count[idx]
+      }
       
       # Update the 2 matrices for regression
       mat_reg1_XX_XY <- updateSuffStat(mat_reg1_XX_XY, fare_less_toll, matrix(trip_time))
       mat_reg2_XX_XY <- updateSuffStat(mat_reg2_XX_XY, fare_less_toll, cbind(trip_time, fares[,2]))
-#    }
+    }
     
-#  },
-#   error=function(cond) {
-#     message("Appears to be at the end of file")
-#     message("Here's the original warning message:")
-#     message(paste(cond, "\n"))
-#     return()
-#   },
-#   finally={
-#     close(connection_data)
-#     close(connection_fare)
-#   }
-# )
+  },
+   error=function(cond) {
+     message("Appears to be at the end of file")
+     message("Here's the original warning message:")
+     message(paste(cond, "\n"))
+     return()
+   },
+   finally={
+     close(connection_data)
+     close(connection_fare)
+   }
+ )
 Rprof(NULL)
 coeff1 <- solve(mat_reg1_XX_XY[, 1 : 2], mat_reg1_XX_XY[, 3])
 coeff2 <- solve(mat_reg2_XX_XY[, 1 : 3], mat_reg2_XX_XY[, 4])
