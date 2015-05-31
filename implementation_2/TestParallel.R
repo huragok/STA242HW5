c8a224099019bf7cc1f1b9c190c6a0d4b6ef81a9
wenhaowu@Loki <wenhaowu1989@hotmail.com>
2015-05-31 11:50:58 -0700

    Running time tests for implementation 2 completed.

--- implementation_2/TestParallel.R
+++ implementation_2/TestParallel.R
@@ -6,7 +6,7 @@ library(parallel)
 idxs <- seq(12) # The indices of the files to be analyzed
 path <- "../data"
 size_bulk <- 500000L
-size_cluster <- 2
+size_cluster <- 12
 
 # The serial version
 #t_serial <- system.time(list_sum_serial <- lapply(idxs, analyzeFile, path, size_bulk, TRUE))
@@ -16,8 +16,9 @@ cl <- makeCluster(size_cluster)
 t_parallel <- system.time(list_sum_parallel <- parLapply(cl, idxs, analyzeFile, path, size_bulk))
 stopCluster(cl)
 
-results_serial <- reduceListSummaryNYCTaxi(list_sum_serial)
-print(results_serial)
+#results_serial <- reduceListSummaryNYCTaxi(list_sum_serial)
+#print(results_serial)
 
 results_parallel <- reduceListSummaryNYCTaxi(list_sum_parallel)
 print(results_parallel)
+print(t_parallel)
